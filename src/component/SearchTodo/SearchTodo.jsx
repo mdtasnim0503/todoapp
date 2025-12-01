ae9869e3e4f5a9a434c1bfe4548fe05a81f72457
MD TASNIM <mdtasnim0503@gmail.com>
2025-12-02 00:48:13 +0530

    first commit 1/12

--- src/component/SearchTodo/SearchTodo.jsx
+++ src/component/SearchTodo/SearchTodo.jsx
@@ -16,7 +16,7 @@ function SearchTodo({ setDrawerOpen, setTasks }) {
     <Box sx={{ display: "flex", justifyContent: "center", height: "100vh" }}>
       <Box
         sx={{
-          width: "50%",
+          width: { xs: "80%", sm: "70%", md: "50%" },
           height: "100%",
           display: "flex",
           flexDirection: "column",
